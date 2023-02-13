8420065a3aca5c40de102a5969adfb7966e5f9cf
Thiago Araujo Nascimento <50850460+TNasci@users.noreply.github.com>
2023-02-13 14:20:29 -0300

    chore: script do banco

---
+++ Scripts/CriacaoBanco_Tabelas.sql
@@ -0,0 +1,35 @@
+create database apoliceSeguros;
+
+use apoliceSeguros;
+
+create table apolice (
+Id int identity(1, 1) primary key,
+descricao VARCHAR(50),
+cpf int,
+situacao VARCHAR (10),
+premio_total decimal(10, 2),
+data_criacao_registro date,
+data_alteracao_registro date,
+usuario_criacao_registro int,
+usuario_alteracao_registro int
+)
+create table parcelas (
+Id int identity(1, 1) primary key,
+apoliceId int,
+premio decimal(10, 2),
+forma_pagamento VARCHAR(50),
+data_pagamento date,
+data_pago date,
+juros decimal(10, 2),
+situacao VARCHAR (10),
+data_criacao_registro date,
+data_alteracao_registro date,
+usuario_criacao_registro int,
+usuario_alteracao_registro int,
+CONSTRAINT FK_Parcelas_Apolice_apoliceId
+ FOREIGN KEY (apoliceId)
+ REFERENCES apolice (id)
+)
+
+select * from apolice a inner join parcelas p on a.id = p.apoliceId;
+select * from parcelas
